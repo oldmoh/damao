f09b4c163fb99e88d37cdb97b3711d29dea11658
mo <yihao_mo@cbnext.co.jp>
2020-11-27 10:03:30 +0900

    feat: add timeline of about page
    在關於頁面新增時間軸

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,14 +6,38 @@ import Layout from '../components/layout'
 import style from './about.module.scss'
 
 const About = ({ data }) => {
+  const { about } = data
+  const activities = about.timeline.activities
+    .sort((a, b) => new Date(b.time) - new Date(a.time))
+    .map((item, index) => {
+      return (
+        <p key={index} className={style.activity}>
+          {item.time}-{item.activity}
+        </p>
+      )
+    })
+
   return (
     <Layout>
-      <SEO title="關於" description={data.about.about_site} />
+      <SEO title="關於" description={about.about_site} />
       <div className={style.about}>
-        <h2>關於大貓日誌</h2>
-        <p>{data.about.about_site}</p>
-        <h2>關於大貓</h2>
-        <p>{data.about.about_me}</p>
+        <div className={style.container}>
+          <h2>關於大貓日誌</h2>
+          <p>{about.about_site}</p>
+        </div>
+        <div className={style.container}>
+          <h2>關於大貓</h2>
+          <p>{about.about_me}</p>
+        </div>
+        <div className={style.container}>
+          <h2>時間軸</h2>
+          <div className={style.timeline}>
+            <p className={style.activity}>
+              <strong>Now</strong>
+            </p>
+            {activities}
+          </div>
+        </div>
       </div>
     </Layout>
   )
@@ -26,6 +50,12 @@ export const query = graphql`
     about: strapiAboutMe {
       about_me
       about_site
+      timeline {
+        activities {
+          activity
+          time
+        }
+      }
     }
   }
 `
